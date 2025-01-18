3c416df5f896ccbf1ac9f3e2a9fd3c99aff43000
Rishi-cs30 <rishisiddharth.sha1@rbe.sk.ca>
2025-01-18 21:34:05 +0000

    added header

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+"""
+Pokémon Adventure Game
 
+This program is a text-based Pokémon game where players can capture and battle Pokémon. 
+Players can explore the wild, challenge gym leaders, and earn badges to become a Pokémon master.
+
+Version: 7.0 - Final Version
+Date: January 16, 2025
+
+Author: Rishi Siddharth Shah
+"""
 from game import Game
 
 if __name__ == "__main__":
